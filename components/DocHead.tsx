1b165fb4bf86424f895ba56a71553c6a654c0b3f
Kevin Latusinski <me@kevlatus.de>
2021-01-08 20:36:27 +0100

    fix: add as attribute to preload

--- components/DocHead.tsx
+++ components/DocHead.tsx
@@ -2,14 +2,18 @@ import Head from "next/head";
 import { FunctionComponent } from "react";
 
 const DocHead: FunctionComponent = function () {
-	return (
-		<Head>
-			<title>Kevin Latusinski</title>
-			<link rel="icon" href="/favicon.ico" />
-      <link rel="preload" href="https://use.typekit.net/xhs5wzt.css" />
-			<link rel="stylesheet" href="https://use.typekit.net/xhs5wzt.css" />
-		</Head>
-	);
+  return (
+    <Head>
+      <title>Kevin Latusinski</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="preload"
+        href="https://use.typekit.net/xhs5wzt.css"
+        as="style"
+      />
+      <link rel="stylesheet" href="https://use.typekit.net/xhs5wzt.css" />
+    </Head>
+  );
 };
 
 export default DocHead;
