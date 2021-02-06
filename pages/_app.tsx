0b8abb35919e5bd8cea5be166cc2c3be9a981b76
Kevin Latusinski <me@kevlatus.de>
2021-02-06 17:56:32 +0100

    feat: switch out matomo for fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as Fathom from "fathom-client";
+
 import "../styles/globals.css";
 
 function KevlatusApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    Fathom.load("JIWYJDVS", {
+      includedDomains: ["www.kevlatus.de", "kevlatus.de"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
