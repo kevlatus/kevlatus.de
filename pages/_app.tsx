3bd9bb1bd1dc74a2ebd11e0430c01e1ceba601db
Kevin Latusinski <me@kevlatus.de>
2021-02-07 17:14:18 +0100

    feat: use plausible instead of fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,13 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import * as Fathom from "fathom-client";
+import PlausibleProvider from "next-plausible";
 
 import "../styles/globals.css";
 
 function KevlatusApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    Fathom.load("JIWYJDVS", {
-      url: "https://mosquito.kevlatus.de/script.js",
-      includedDomains: ["www.kevlatus.de", "kevlatus.de"],
-    });
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-
-    router.events.on("routeChangeComplete", onRouteChangeComplete);
-
-    return () => {
-      router.events.off("routeChangeComplete", onRouteChangeComplete);
-    };
-  }, []);
-
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain="www.kevlatus.de">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
 
 export default KevlatusApp;
